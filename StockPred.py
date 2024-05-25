8691cce888735882d4e061098f1a83d5b22964d5
Rutuja Jadhav <160432263+rutuja-jadhav-github@users.noreply.github.com>
2024-05-25 15:29:56 +0100

    Update StockPred.py
    Text

--- StockPred.py
+++ StockPred.py
@@ -20,7 +20,7 @@ st.title("Stock Prediction App")
 st.subheader('by Rutuja Jadhav')
 st.header('About')
 st.write('This app was created as a Portfolio project to predict the stock prices from a given selection of tickers. You can read more about this project on the README file available on my Git Hub here - https://github.com/rutuja-jadhav-github/Stock_Prediction_App/blob/main/README.md. If you like this work, please leave me a star on the GitHub repository :) To learn more about building data apps for your portfolio check out @patloeber on You Tube.')
-
+st.header(' ')
 
 stocks = ('AAPL','GOOG','MSFT','GME')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
